8e140568a235342a1d6d94f930ad8ffa5f923e41
lma_dev <maximus-lihotin@mail.ru>
2026-04-08 01:06:34 +0300

    fix

--- main.js
+++ main.js
@@ -46,9 +46,19 @@ function initEditor(roomName) {
         iceServers: [
           { urls: 'stun:stun.l.google.com:19302' },
           { urls: 'stun:stun1.l.google.com:19302' },
-          { urls: 'stun:stun2.l.google.com:19302' }
+          {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:443',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          }
         ]
-      }
+      },
+      debug: 2  // Enable debug logging
     }
   })
 
